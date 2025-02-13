03825e9fee5bd70bb059c50aebf75ffaf637fbc3
Dra825 <gagasuyu@gmail.com>
2025-02-13 09:32:15 +0100

    Internal

--- src/db-conection.ts
+++ src/db-conection.ts
@@ -8,7 +8,7 @@ import { Pool } from 'pg';
   database: 'proyecto'
 });*/
 
-const connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game'
+const connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a/cartas_game'
 
 
 const pool = new Pool({
