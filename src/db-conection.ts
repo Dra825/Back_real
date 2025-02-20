59445ce362c74ebf1b72d1b7d9eb4360c2b9e014
Dra825 <gagasuyu@gmail.com>
2025-02-20 18:26:10 +0100

    Add files via upload

--- src/db-conection.ts
+++ src/db-conection.ts
@@ -1,14 +1,15 @@
 import { Pool } from 'pg';
 
-const pool = new Pool ({
 
-    user: 'postgres',
-    password: '1234',
-    host: 'localhost',
-    port: 5432, //Default Postgress port
-    database: 'Juego'
-});
+
+
+const connectionString = 
+'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game'
+
+const pool = new Pool({
+    connectionString,
+})
 
 export function query(text: any): any {
     return pool.query(text);
-}
+};
