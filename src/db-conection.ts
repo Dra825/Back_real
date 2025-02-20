c8bfecea35893c875c1831edeca985e4d42baf2e
Dra825 <gagasuyu@gmail.com>
2025-02-20 21:10:04 +0100

    Add files via upload

--- src/db-conection.ts
+++ src/db-conection.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 
 
-const connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game'
+const connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game?sslmode=require'
 
 
 const pool = new Pool({
@@ -11,4 +11,4 @@ const pool = new Pool({
 
 export function query(text: any): any {
     return pool.query(text);
-};
+}; 
